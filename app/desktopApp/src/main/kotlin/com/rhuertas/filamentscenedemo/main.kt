85b51649b85f334f62247a73d1b13446c5384914
Raul Huertas <rax20037@gmail.com>
2026-08-27 17:06:07 -0500

    Solving arm64 issues

--- app/desktopApp/src/main/kotlin/com/rhuertas/filamentscenedemo/main.kt
+++ app/desktopApp/src/main/kotlin/com/rhuertas/filamentscenedemo/main.kt
@@ -4,7 +4,14 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    System.setProperty("skiko.renderApi", "OPENGL")
+    val osName = System.getProperty("os.name") ?: ""
+    val osArch = System.getProperty("os.arch") ?: ""
+    val isLinuxArm64 = osName.contains("Linux", ignoreCase = true) &&
+        (osArch == "arm64" || osArch == "aarch64")
+
+    if (isLinuxArm64) {
+        System.setProperty("skiko.renderApi", "OPENGL")
+    }
 
     Window(
         onCloseRequest = ::exitApplication,
